8787dad8734edce2cc76cda6f2c2e8460af38dc9
Brancucci <brancucci@mail.usf.edu>
2017-04-15 09:37:42 -0400

    successfully added a user to database with register

--- Touring360/views.py
+++ Touring360/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 #Importing needed modules of PyMySQL
 from django.http import JsonResponse
 import json
+import time
 
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
@@ -128,6 +129,36 @@ def login(request):
                 }))
     else:
         return HttpResponse("You must pass parameters (A body) to this url")
+
+
+@method_decorator(csrf_exempt)
+def register(request):
+    if request.method == 'POST':
+        try:
+            body_unicode = request.body.decode('utf-8')
+            body = json.loads(body_unicode)
+            user = body['email']
+            password = body['password']
+            phone = body['phone']
+            city = body['city']
+            country = body['country']
+            address = body['address']
+            firstName = body['firstName']
+            lastName = body['lastName']
+            date = time.strftime("%Y-%m-%d")
+            cursor = conn.cursor(cursors.DictCursor);
+            # Create a new record
+            sql = "INSERT INTO USERS (email, password, signup_date, phone, city, country, address, first_name, last_name) " \
+                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (user, password, date, phone, city, country, address, firstName, lastName))
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            conn.commit()
+            return HttpResponse("Success")
+
+        except KeyError:
+            return HttpResponse("Key error 1")
 
 
 
