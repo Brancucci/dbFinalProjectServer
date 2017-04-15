c607d1b2c5fd3a8c949acd82e4d9641dba6597fb
Brancucci <brancucci@mail.usf.edu>
2017-04-15 09:55:29 -0400

    cleaned up registration code before pushing to git

--- Touring360/views.py
+++ Touring360/views.py
@@ -1,16 +1,14 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
-#Importing needed modules of PyMySQL
 from django.http import JsonResponse
 import json
 import time
-
 from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 from pymysql import connect, err, sys, cursors
 
 
-#Doing our connection
+#our connection
 conn = connect(host='mysqldb.criblyzj9wkn.us-west-2.rds.amazonaws.com',
                         port=3306,
                         user='admin',
@@ -21,43 +19,6 @@ conn = connect(host='mysqldb.criblyzj9wkn.us-west-2.rds.amazonaws.com',
 def index(request):
     return HttpResponse("My home page")
 
-    # if request.method == 'POST':
-    #     try:
-    #         body_unicode = request.body.decode('utf-8')
-    #         body = json.loads(body_unicode)
-    #         user = body['email']
-    #         password = body['password']
-    #         cursor = conn.cursor(cursors.DictCursor);
-    #         cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", str(user))
-    #         data = cursor.fetchall()
-    #     except KeyError:
-    #         return HttpResponse("Key Error")
-    #     else:
-    #         if data:
-    #             if data[0]["password"] != password:
-    #                 # wrong password
-    #                 return HttpResponse(JsonResponse({
-    #                     'notFound': False,
-    #                     'wrongPassword': True,
-    #                     'success': False,
-    #                 }))
-    #             else:
-    #                 # user password matches password
-    #                 return HttpResponse(JsonResponse({
-    #                     'notFound': False,
-    #                     'wrongPassword': False,
-    #                     'success': True,
-    #                     }))
-    #         else:
-    #             return HttpResponse(JsonResponse({
-    #                 'notFound': True,
-    #                 'wrongPassword': False,
-    #                 'success': False,
-    #             }))
-    #
-    #     return HttpResponse(data)
-    # else:
-    #     return HttpResponse("Was not a post method")
 
     """
     if request.method == 'GET':
@@ -79,16 +40,6 @@ def index(request):
 
     conn.close()
 
-"""
-"""
-to get:
-try --- request.content.key
-
-to respond:
->>> from django.http import JsonResponse
->>> response = JsonResponse({'foo': 'bar'})
->>> response.content
-b'{"foo": "bar"}'
 """
 
 @method_decorator(csrf_exempt)
@@ -134,6 +85,7 @@ def login(request):
 @method_decorator(csrf_exempt)
 def register(request):
     if request.method == 'POST':
+        # get the body of the post request
         try:
             body_unicode = request.body.decode('utf-8')
             body = json.loads(body_unicode)
@@ -146,52 +98,72 @@ def register(request):
             firstName = body['firstName']
             lastName = body['lastName']
             date = time.strftime("%Y-%m-%d")
-            cursor = conn.cursor(cursors.DictCursor);
-            # Create a new record
-            sql = "INSERT INTO USERS (email, password, signup_date, phone, city, country, address, first_name, last_name) " \
-                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            cursor.execute(sql, (user, password, date, phone, city, country, address, firstName, lastName))
-
-            # connection is not autocommit by default. So you must commit to save
-            # your changes.
-            conn.commit()
-            return HttpResponse("Success")
 
         except KeyError:
-            return HttpResponse("Key error 1")
+            return HttpResponse(JsonResponse({
+                'error': True,
+                'taken': False,
+                'success': False,
+            }))
+        else:
+            # check user exists
+            cursor = conn.cursor(cursors.DictCursor);
+            cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", user)
+            data = cursor.fetchall()
+            if data:
+                #user already exists
+                return HttpResponse(JsonResponse({
+                    'error': False,
+                    'taken': True,
+                    'success': False,
+                }))
+            else:
+                # Create a new record
+                sql = "INSERT INTO USERS (email, password, signup_date, phone, city, country, address, first_name, last_name) " \
+                      "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (user, password, date, phone, city, country, address, firstName, lastName))
+
+                # connection is not autocommit by default. So you must commit to save
+                # your changes.
+                conn.commit()
+                return HttpResponse(JsonResponse({
+                    'error': False,
+                    'taken': False,
+                    'success': True,
+                }))
+    else:
+        return HttpResponse("You must pass parameters (A body) to this url")
 
 
-
-
-            # try:
-            #     user = request.GET['email']
-            #     password = request.GET['password']
-            #     cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", user)
-            #     data = cursor.fetchall()
-            # except KeyError:
-            #     return HttpResponse("Key Error 2")
-            # else:
-            #     if data:
-            #         if data[0]["password"] != password:
-            #             # wrong password
-            #             return HttpResponse(JsonResponse({
-            #                 'notFound': False,
-            #                 'wrongPassword': True,
-            #                 'success': False,
-            #             }))
-            #         else:
-            #             # user password matches password
-            #             return HttpResponse(JsonResponse({
-            #                 'notFound': False,
-            #                 'wrongPassword': False,
-            #                 'success': True,
-            #                 }))
-            #     else:
-            #         return HttpResponse(JsonResponse({
-            #             'notFound': True,
-            #             'wrongPassword': False,
-            #             'success': False,
-            #         }))
+# try:
+#     user = request.GET['email']
+#     password = request.GET['password']
+#     cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", user)
+#     data = cursor.fetchall()
+# except KeyError:
+#     return HttpResponse("Key Error 2")
+# else:
+#     if data:
+#         if data[0]["password"] != password:
+#             # wrong password
+#             return HttpResponse(JsonResponse({
+#                 'notFound': False,
+#                 'wrongPassword': True,
+#                 'success': False,
+#             }))
+#         else:
+#             # user password matches password
+#             return HttpResponse(JsonResponse({
+#                 'notFound': False,
+#                 'wrongPassword': False,
+#                 'success': True,
+#                 }))
+#     else:
+#         return HttpResponse(JsonResponse({
+#             'notFound': True,
+#             'wrongPassword': False,
+#             'success': False,
+#         }))
 
 
 
