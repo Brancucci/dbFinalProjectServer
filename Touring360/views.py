bc15bfcebb4307514652be7d4f07b186f8ba9993
Brancucci <brancucci@mail.usf.edu>
2017-04-14 22:29:07 -0400

    have login with appropriate errors

--- Touring360/views.py
+++ Touring360/views.py
@@ -16,12 +16,29 @@ conn = connect(host='mysqldb.criblyzj9wkn.us-west-2.rds.amazonaws.com',
 
 
 def index(request):
-    cursor = conn.cursor(cursors.DictCursor);
-    user = 'sometourist@mail.com'
-    cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", user)
-    data = cursor.fetchall()
-    return HttpResponse(data)
+    return HttpResponse("My home page!!")
 
+    """
+    if request.method == 'GET':
+        cursor = conn.cursor(cursors.DictCursor);
+        user = request.GET['email']
+        cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", user)
+        data = cursor.fetchall()
+
+        return HttpResponse("Testing: " + data[0]["email"] + " password" + data[0]["password"])
+    elif request.method == 'POST':
+        user = request.POST['email']
+        password = request.POST['password']
+        cursor = conn.cursor(cursors.DictCursor);
+        cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", str(user))
+        data = cursor.fetchall()
+        return HttpResponse(data)
+    else:
+        return HttpResponse("failure")
+
+    conn.close()
+
+"""
 """
 to get:
 try --- request.content.key
@@ -35,19 +52,44 @@ b'{"foo": "bar"}'
 
 
 def login(request):
-    user = request.GET.email
-    password = request.GET.password
-    cursor = conn.cursor(cursors.DictCursor);
-    cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", user)
-    data = cursor.fetchall()
-    print(data)
-    if not data[0].password == password:
-        #wrong password
-        pass
-    else:
-        # user password matches password
-        return HttpResponse(JsonResponse({
-            'notFound': False,
-            'wrongPassword': False,
-            'success': True,
-            'user': data[0]}))
+    user = ""
+    if request.method == 'GET':
+        cursor = conn.cursor(cursors.DictCursor);
+        try:
+            user = request.GET['email']
+            password = request.GET['password']
+            cursor.execute("SELECT * FROM `USERS` WHERE `email` = %s", user)
+            data = cursor.fetchall()
+        except KeyError:
+            return HttpResponse("Key Error")
+        else:
+            if data:
+                if data[0]["password"] != password:
+                    # wrong password
+                    return HttpResponse(JsonResponse({
+                        'notFound': False,
+                        'wrongPassword': True,
+                        'success': False,
+                    }))
+                else:
+                    # user password matches password
+                    return HttpResponse(JsonResponse({
+                        'notFound': False,
+                        'wrongPassword': False,
+                        'success': True,
+                        }))
+            else:
+                return HttpResponse(JsonResponse({
+                    'notFound': True,
+                    'wrongPassword': False,
+                    'success': False,
+                }))
+
+
+
+
+
+
+
+
+
